315b6753e0524d4688f767ad24814c7d1643eb1a
Kalmeshjujagar <Kalmeshjujagar@gmail.com>
2024-07-13 21:13:28 +0530

    variable and datatypes

---
+++ 01varAndData.js
@@ -0,0 +1,34 @@
+// Activity 01
+var thaar = 5055;
+let docomo = "This is string"
+
+console.log(thaar);
+console.log(docomo);
+
+const isLoding = true;
+console.log(isLoding);
+
+
+const one = 1
+const two = "Hello world!"
+const three = false
+const four = {
+    one: 1,
+    two: 2,
+    three: 3
+}
+const five = ["apple", "orange", "banana"]
+
+console.log(typeof(one))
+console.log(typeof(two))
+console.log(typeof(three))
+console.log(typeof(four))
+console.log(typeof(five))
+
+let reassign = "initial value to let operators"
+reassign = "this is reassigned value let operator"
+console.log(reassign)
+
+const newValue = "this initial const value"
+// newValue = "this is reassing value to const operator"
+console.log(newValue);
